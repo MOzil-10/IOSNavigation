a105c668263d01726eec9acd5b595c85daca028e
Mukosi <mukosibudeli@gmail.com>
2023-06-12 08:37:47 +0200

    first commit

---
+++ Navigation/HomeVC.swift
@@ -0,0 +1,32 @@
+//
+//  HomeVC.swift
+//  Navigation
+//
+//  Created by DA MAC M1 115 on 2023/05/19.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func firstB(_ sender: UIButton) {
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Navigation/SecondViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SecondViewController.swift
+//  Navigation
+//
+//  Created by DA MAC M1 115 on 2023/05/19.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func btnSecond(_ sender: Any) {
+        let storybpad = self.storyboard?.instantiateViewController(identifier: "ThirdViewController") as? ThirdViewController
+        self.navigationController.pushViewController?(storyboard, animated: true)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
